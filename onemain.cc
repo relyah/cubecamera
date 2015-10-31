4118216a0fd4326a822ec7961df76069e04ab25b
Loic Perrot <relyah@hotmail.com>
2015-10-31 16:17:03 +0200

    working

--- onemain.cc
+++ onemain.cc
@@ -69,7 +69,7 @@ int main() {
   //glPolygonMode(GL_FRONT, GL_LINE); //for wireframe
   //glPolygonMode(GL_BACK, GL_LINE); //for wireframe
   glClear(GL_DEPTH_BUFFER_BIT);
-  glClearColor(0.0f, 0.0f, 0.1f, 1.0f);
+  //glClearColor(0.0f, 0.0f, 0.1f, 1.0f);
   //------------------------------------------------------------------
 
   //------------------------------------------------------------------
@@ -102,7 +102,7 @@ int main() {
 
   numPoints = 4;
   VertexStructure points[] = {
-  0.5f,  0.5f,  0.5f, 0.0f,0.0f,1.0f, 1.0f,0.0f,0.0f,
+    0.5f,  0.5f,  0.5f, 0.0f,0.0f,1.0f, 1.0f,0.0f,0.0f,
     0.5f, -0.5f,  0.5f, 0.0f,0.0f,1.0f, 1.0f,0.0f,0.0f,
     -0.5f, -0.5f, 0.5f, 0.0f,0.0f,1.0f, 1.0f,0.0f,0.0f,
     -0.5f, 0.5f, 0.5f, 0.0f,0.0f,1.0f, 1.0f,0.0f,0.0f,
@@ -117,11 +117,11 @@ int main() {
     -0.5f, 0.5f, 0.5f, -1.0f,0.0f,0.0f, 1.0f,0.0f,0.0f,
     -0.5f, 0.5f, -0.5f, -1.0f,0.0f,0.0f, 0.0f,1.0f,0.0f
 
-    };
+  };
 
   numIndices = 6;
   unsigned short pointsIndex[] = {
-  0,1,2,2,3,0,
+    0,1,2,2,3,0,
     4,6,5,6,4,7,
     8,9,10,10,9,11};
 
@@ -151,10 +151,6 @@ int main() {
 
   //------------------------------------------------------------------
 
-  glGenFramebuffers(1, &fboTV);
-  glBindFramebuffer(GL_FRAMEBUFFER, fboTV);
-  cout << "fboTV: "<<fboTV <<endl;
-  //glActiveTexture(GL_TEXTURE0);
   glGenTextures(1, &color_textureTV);
   cout << "color_texture: "<<color_textureTV <<endl;
   glBindTexture(GL_TEXTURE_2D, color_textureTV);
@@ -165,30 +161,33 @@ int main() {
 
   unsigned char* data = (unsigned char*) malloc(size * size * 4);
 	for (int col = 0; col < size; col++) {
-  float alpha = (float) M_PI * col * 360.0f / ((float) size * 180.0f);
-  for (int row = 0; row < size; row++) {
-  float beta = (float) M_PI * row * 360.0f / ((float) size * 180.0f);
-  int index = (col * size + row) * 4;
-  data[index] = (unsigned char) (255.0f * sin(alpha));
-  data[index + 1] = (unsigned char) (255.0f * cos(beta));
-  data[index + 2] = (unsigned char) (255.0f * sin(beta) * cos(alpha));
-  data[index + 3] = 255;
-}
-}
+    float alpha = (float) M_PI * col * 360.0f / ((float) size * 180.0f);
+    for (int row = 0; row < size; row++) {
+      float beta = (float) M_PI * row * 360.0f / ((float) size * 180.0f);
+      int index = (col * size + row) * 4;
+      data[index] = (unsigned char) (255.0f * sin(alpha));
+      data[index + 1] = (unsigned char) (255.0f * cos(beta));
+      data[index + 2] = (unsigned char) (255.0f * sin(beta) * cos(alpha));
+      data[index + 3] = 255;
+    }
+  }
 	glTexImage2D(GL_TEXTURE_2D, // target
-    0,  // level, 0 = base, no minimap,
-    GL_RGBA, // internalformat
-    size,  // width
-    size,  // height
-    0,  // border, always 0 in OpenGL ES
-    GL_RGBA,  // format
-    GL_UNSIGNED_BYTE, // type
-    data);
+               0,  // level, 0 = base, no minimap,
+               GL_RGBA, // internalformat
+               size,  // width
+               size,  // height
+               0,  // border, always 0 in OpenGL ES
+               GL_RGBA,  // format
+               GL_UNSIGNED_BYTE, // type
+               data);
 	free(data);
   //glGenTextures(1, &depth_texture);
   //glBindTexture(GL_TEXTURE_2D, depth_texture);
   //glTexStorage2D(GL_TEXTURE_2D, 9, GL_DEPTH_COMPONENT32F, 512, 512);
 
+  glGenFramebuffers(1, &fboTV);
+  glBindFramebuffer(GL_FRAMEBUFFER, fboTV);
+  cout << "fboTV: "<<fboTV <<endl;
   //glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, color_textureTV, 0);
   glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, color_textureTV, 0);
   //glFramebufferTexture(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, depth_textureTV, 0);
@@ -211,52 +210,18 @@ int main() {
   uniform_mTV = GetUniform(programTV,"model");
   uniform_vTV = GetUniform(programTV,"view");
   uniform_pTV = GetUniform(programTV,"projection");
-  
-  /*glGenTextures(1, &color_textureTV);
-    glBindTexture(GL_TEXTURE_2D, color_textureTV);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    size = 512;
-    glTexStorage2D(GL_TEXTURE_2D, 0, GL_RGBA8, size,size);
-
-    data = (unsigned char*) malloc(size * size * 4);
-    for (int col = 0; col < size; col++) {
-    float alpha = (float) M_PI * col * 360.0f / ((float) size * 180.0f);
-    for (int row = 0; row < size; row++) {
-    float beta = (float) M_PI * row * 360.0f / ((float) size * 180.0f);
-    //std::cout << (col * size + row)*4 << " " << count << std::endl;
-    int index = (col * size + row) * 4;
-    data[index] = (unsigned char) (255.0f * sin(alpha));
-    data[index + 1] = (unsigned char) (255.0f * cos(beta));
-    data[index + 2] = (unsigned char) (255.0f * sin(beta) * cos(alpha));
-    data[index + 3] = 255;
-    }
-    }
-
-    glTexImage2D(GL_TEXTURE_2D, // target
-    0,  // level, 0 = base, no minimap,
-    GL_RGBA, // internalformat
-    size,  // width
-    size,  // height
-    0,  // border, always 0 in OpenGL ES
-    GL_RGBA,  // format
-    GL_UNSIGNED_BYTE, // type
-    data);
-
-    free(data);
-  */
 
   numPointsTV = 4;
   TVStructure pointsTV[] = {
-  1.0f,  0.0f,  0.1f, 1.0f,0.0f,
+    1.0f,  0.0f,  0.1f, 1.0f,0.0f,
     1.0f, -1.0f,  0.1f, 1.0f,1.0f,
     0.0f, -1.0f, 0.1f, 0.0f,1.0f,
     0.0f, 0.0f, 0.1f, 0.0f,0.0f
-    };
+  };
 
   numIndicesTV = 6;
   unsigned short pointsIndexTV[] = {
-  0,1,2,2,3,0};
+    0,1,2,2,3,0};
 
   glGenBuffers(1, &vboPointsTV);
   glGenBuffers(1, &iboPointsTV);
@@ -284,7 +249,7 @@ int main() {
   //------------------------------------------------------------------
   glUseProgram(program);
   glBindVertexArray(vao);
-  glBindFramebuffer(GL_FRAMEBUFFER, fboTV);
+  glBindFramebuffer(GL_FRAMEBUFFER, fboTV); //drawing into framebuffer
 
   //logger->info("Square updating...");
   glm::mat4 modelMatrix = glm::mat4(1.0f);
@@ -306,7 +271,7 @@ int main() {
   //------------------------------------------------------------------
   glUseProgram(programTV);
   glBindVertexArray(vaoTV);
-  glBindFramebuffer(GL_FRAMEBUFFER, 0);
+  glBindFramebuffer(GL_FRAMEBUFFER, 0); //go back to default framebuffer
 
   modelMatrix = glm::mat4(1.0f);
   glUniformMatrix4fv(uniform_mTV,1,GL_FALSE,glm::value_ptr(modelMatrix));
@@ -337,7 +302,7 @@ int main() {
   //------------------------------------------------------------------
 }
 
-  GLuint CreateShader(GLenum type, const char *filename) {
+GLuint CreateShader(GLenum type, const char *filename) {
   GLuint s = glCreateShader(type);
   char* src = file_read(filename);
   //logger->info(src);
@@ -346,39 +311,39 @@ int main() {
   GLint compile_ok = GL_FALSE;
   glGetShaderiv(s, GL_COMPILE_STATUS, &compile_ok);
   if (compile_ok == GL_FALSE) {
-  cout << filename << endl;
-  //fprintf(stderr, "%s:", filename);
-  print_log(s);
-  glDeleteShader(s);
-  return 0;
-}
+    cout << filename << endl;
+    //fprintf(stderr, "%s:", filename);
+    print_log(s);
+    glDeleteShader(s);
+    return 0;
+  }
   return s;
 }
 
-  void print_log(GLuint object) {
+void print_log(GLuint object) {
 	GLint log_length = 0;
 	if (glIsShader(object)) {
-  glGetShaderiv(object, GL_INFO_LOG_LENGTH, &log_length);
-} else if (glIsProgram(object)) {
-  glGetProgramiv(object, GL_INFO_LOG_LENGTH, &log_length);
-} else {
-  cout << "printlog: Not a shader or a program" << endl;
-  return;
-}
+    glGetShaderiv(object, GL_INFO_LOG_LENGTH, &log_length);
+  } else if (glIsProgram(object)) {
+    glGetProgramiv(object, GL_INFO_LOG_LENGTH, &log_length);
+  } else {
+    cout << "printlog: Not a shader or a program" << endl;
+    return;
+  }
 
   char* log = (char*) malloc(log_length);
 
   if (glIsShader(object)) {
-  glGetShaderInfoLog(object, log_length, NULL, log);
-} else if (glIsProgram(object)) {
-  glGetProgramInfoLog(object, log_length, NULL, log);
-}
+    glGetShaderInfoLog(object, log_length, NULL, log);
+  } else if (glIsProgram(object)) {
+    glGetProgramInfoLog(object, log_length, NULL, log);
+  }
 
   cout << log << endl;
   free(log);
 }
 
-  char* file_read(const char* filename) {
+char* file_read(const char* filename) {
 
 	FILE* in = fopen(filename, "rb");
 	if (in == NULL)
@@ -387,16 +352,16 @@ int main() {
 	char* res = (char*) malloc(res_size);
 	int nb_read_total = 0;
 	while (!feof(in) && !ferror(in)) {
-  if (nb_read_total + BUFSIZ > res_size) {
-  if (res_size > 10 * 1024 * 1024) {
-  break;
-}
-  res_size = res_size * 2;
-  res = (char*) realloc(res, res_size);
-}
-  char* p_res = res + nb_read_total;
-  nb_read_total += fread(p_res, 1, BUFSIZ, in);
-}
+    if (nb_read_total + BUFSIZ > res_size) {
+      if (res_size > 10 * 1024 * 1024) {
+        break;
+      }
+      res_size = res_size * 2;
+      res = (char*) realloc(res, res_size);
+    }
+    char* p_res = res + nb_read_total;
+    nb_read_total += fread(p_res, 1, BUFSIZ, in);
+  }
 	fclose(in);
 	res = (char*) realloc(res, nb_read_total + 1);
 	res[nb_read_total] = '\0';
@@ -404,42 +369,42 @@ int main() {
 
 }
 
-  GLuint CreateProgram(const char *vertexfile, const char *fragmentfile, GLuint &vshader, GLuint &fshader) {
+GLuint CreateProgram(const char *vertexfile, const char *fragmentfile, GLuint &vshader, GLuint &fshader) {
 	GLuint program = glCreateProgram();
 
   if (vertexfile) {
-  vshader = CreateShader(GL_VERTEX_SHADER,vertexfile);
-  if (!vshader)
-    return 0;
-  glAttachShader(program, vshader);
-}
+    vshader = CreateShader(GL_VERTEX_SHADER,vertexfile);
+    if (!vshader)
+      return 0;
+    glAttachShader(program, vshader);
+  }
 	if (fragmentfile) {
-  fshader = CreateShader(GL_FRAGMENT_SHADER,fragmentfile);
-  if (!fshader)
-    return 0;
-  glAttachShader(program, fshader);
-}
+    fshader = CreateShader(GL_FRAGMENT_SHADER,fragmentfile);
+    if (!fshader)
+      return 0;
+    glAttachShader(program, fshader);
+  }
 	glLinkProgram(program);
 	GLint link_ok = GL_FALSE;
 	glGetProgramiv(program, GL_LINK_STATUS, &link_ok);
 	if (!link_ok) {
-  cout << "glLinkProgram:" << endl;
-  print_log(program);
-  glDeleteProgram(program);
-  return 0;
-}
+    cout << "glLinkProgram:" << endl;
+    print_log(program);
+    glDeleteProgram(program);
+    return 0;
+  }
 
   return program;
 }
 
-  GLint GetAttrib(GLuint program, const char *name) {
+GLint GetAttrib(GLuint program, const char *name) {
 	GLint attribute = glGetAttribLocation(program, name);
 	if (attribute == -1)
 		fprintf(stderr, "Could not bind attribute %s\n", name);
 	return attribute;
 }
 
-  GLint GetUniform(GLuint program, const char *name) {
+GLint GetUniform(GLuint program, const char *name) {
 	GLint uniform = glGetUniformLocation(program, name);
 	if (uniform == -1)
 		fprintf(stderr, "Could not bind uniform %s\n", name);
